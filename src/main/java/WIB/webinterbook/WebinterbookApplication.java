0476d39157c47dba362b50b153f43f5727157249
이은우 <80537533+lew0205@users.noreply.github.com>
2023-04-27 20:34:02 +0900

    init project

---
+++ src/main/java/WIB/webinterbook/WebinterbookApplication.java
@@ -0,0 +1,13 @@
+package WIB.webinterbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebinterbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebinterbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/WIB/webinterbook/WebinterbookApplicationTests.java
@@ -0,0 +1,13 @@
+package WIB.webinterbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebinterbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
