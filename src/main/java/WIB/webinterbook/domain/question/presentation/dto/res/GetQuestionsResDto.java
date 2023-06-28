2e40e070a523468282b19e823e691a4a23544841
이은우 <80537533+lew0205@users.noreply.github.com>
2023-06-28 17:37:57 +0900

    질문들 받아오는 기능 질문의 답은 받아오지 않게 변경

--- src/main/java/WIB/webinterbook/domain/question/presentation/dto/res/GetQuestionsResDto.java
+++ src/main/java/WIB/webinterbook/domain/question/presentation/dto/res/GetQuestionsResDto.java
@@ -1,14 +1,31 @@
 package WIB.webinterbook.domain.question.presentation.dto.res;
 
 import WIB.webinterbook.domain.question.Question;
+import WIB.webinterbook.domain.question.QuestionTag;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
-@AllArgsConstructor
 public class GetQuestionsResDto {
 
-    List<Question> questions;
+    private List<SimpleQuestionInfo> questions;
+
+    public GetQuestionsResDto(List<Question> datas) {
+        datas.forEach(data -> questions.add(new SimpleQuestionInfo(data)));
+    }
+}
+
+@Getter
+class SimpleQuestionInfo {
+    private final Long id;
+    private final String question;
+    private final QuestionTag tag;
+
+    public SimpleQuestionInfo(Question data) {
+        this.id = data.getId();
+        this.question = data.getQuestion();
+        this.tag = data.getTag();
+    }
 }
