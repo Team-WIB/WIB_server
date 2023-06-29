8c86e88b56497844212660b154e95041e880281e
이은우 <80537533+lew0205@users.noreply.github.com>
2023-06-29 11:09:08 +0900

    build시 생기는 error fix

--- src/test/java/WIB/webinterbook/WebinterbookApplicationTests.java
+++ src/test/java/WIB/webinterbook/WebinterbookApplicationTests.java
@@ -3,7 +3,7 @@ package WIB.webinterbook;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class WebinterbookApplicationTests {
 
 	@Test
