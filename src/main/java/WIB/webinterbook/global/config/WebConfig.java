e2ded0f672e7b1dd8da0ac37f7a4c4e70718c0a7
이은우 <80537533+lew0205@users.noreply.github.com>
2023-06-30 09:51:49 +0900

    cors error fix

---
+++ src/main/java/WIB/webinterbook/global/config/WebConfig.java
@@ -0,0 +1,15 @@
+package WIB.webinterbook.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://ec2-3-39-199-70.ap-northeast-2.compute.amazonaws.com:8080")
+            .allowedMethods("GET", "POST", "PUT");
+    }
+}
